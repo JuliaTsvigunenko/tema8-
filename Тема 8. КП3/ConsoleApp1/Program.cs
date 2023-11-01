54bebc5fb80fa586f9b40081d5ed18090a71ec64
JuliaTsvigunenko <122933386+JuliaTsvigunenko@users.noreply.github.com>
2023-11-02 01:03:34 +0200

    Add files via upload

---
+++ Тема 8. КП3/ConsoleApp1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        //Задание 1.
+        //3. Вывести на экран вертикальную строку из 18 символов, отодвинутую от левого края экрана на 5 позиций.
+        //static void Func()
+        //{
+        //    for ( int i=1; i<=18; i++ )
+        //    {
+        //        Console.WriteLine("     #");
+        //    }
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    Func();
+        //}
+
+
+       
+
+
+
+
+
+        //Задание 3. 
+        //Вычислить значение выражения, определив и использовав дополнительный метод: №3
+        //public static double Func2(double x)
+        //{
+        //    x = (2 + (Math.Sin(2))) / ((Math.Sin(5)) + 5);
+        //    return x;
+        //}
+        //public static double Func6(double x)
+        //{
+        //    x = (6 + (Math.Sin(6))) / ((Math.Sin(3)) + 3);
+        //    return x;
+        //}
+        //public static double Func1(double x)
+        //{
+        //    x = (1 + (Math.Sin(1))) / ((Math.Sin(4)) + 4);
+        //    return x;
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    double x = 0;
+        //    double y = Func2(x) + Func6(x) + Func1(x);
+        //    Console.Write($"y={y}");
+        //}
+
+
+
+
+
+        //Задание 4. 
+        //3. Найти s = max(a, b) + max(c, d), используя вспомогательную функцию(метод).
+
+        //static int Max(int x, int y)
+        //{
+        //    return x > y ? x : y;  //Если "x" больше "y", функция возвращает "x". В противном случае, если "x" не больше "y", функция возвращается y. Это способ выбрать наибольшее значение из двух чисел "x" и "y" вернуть его.
+        //}
+
+        //static void Main()
+        //{
+        //    Console.Write("Введите первое число a: ");
+        //    int a = Convert.ToInt32(Console.ReadLine());
+        //    Console.Write("Введите второе число b: ");
+        //    int b = Convert.ToInt32(Console.ReadLine());
+        //    Console.Write("Введите третье число c: ");
+        //    int c = Convert.ToInt32(Console.ReadLine());
+        //    Console.Write("Введите четвертое число d: ");
+        //    int d = Convert.ToInt32(Console.ReadLine());
+
+        //    int sum = Max(a, b) + Max(c, d);
+
+        //    Console.WriteLine("sum = " + sum);
+        //}
+
+    }
+}
